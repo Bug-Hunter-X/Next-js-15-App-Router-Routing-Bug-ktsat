0d84fbd9579203af380a7f2544f5f0d42d80f4da
BugHunter-X <askzenai@gmail.com>
2025-02-15 15:40:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,25 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+
+// pages/about.js
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+```
